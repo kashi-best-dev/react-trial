dbe63ea5cb3b83001f43eaa2045bca4887bbbbd0
kashi <kashi19122004@gmail.com>
2024-12-17 22:17:26 +0530

    0.0.2
    added card component

---
+++ trial/src/Card/Card.jsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <div>
+      <h1>para 1</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur
+        vitae quas doloribus deleniti debitis autem tempora accusamus adipisci
+        fugiat repellendus. Soluta libero laboriosam sequi debitis esse facilis
+        dolore dicta in.
+      </p>
+    </div>
+  );
+}
+
+export default Card;
